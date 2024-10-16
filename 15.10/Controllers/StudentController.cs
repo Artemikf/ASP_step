528b98bda42f29e3ed52cb6e0d19d8d57a1a9446
Artem Kurta <artemdog386@gmail.com>
2024-10-16 18:28:31 +0300

    first commit

---
+++ 15.10/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using _15._10.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _15._10.Controllers;
+
+public class StudentController : Controller
+{
+    private Student _student = new Student
+    {
+        DateOfBirthday = DateTime.Now,
+        FirtsName = "test_1",
+        LastName = "test_2",
+        GPA = 5
+    };
+
+
+    public IActionResult GetStudents()
+    {
+
+        //ViewBag.Student = _student;
+        //ViewData["Student"] = _student;
+
+        return View(_student);
+    }
+}
---
+++ 15.10/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace _15._10.Models;
+
+public class Student
+{
+    public string FirtsName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirthday { get; set; }
+    public double GPA { get; set; }
+}
